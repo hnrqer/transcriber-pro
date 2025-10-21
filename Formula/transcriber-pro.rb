d28cfd411e19aa709578f00b7e9eecd779eee4d2
Henrique Sarmento <hnrqer@gmail.com>
2025-10-21 02:31:50 -0300

    Add automated Homebrew formula update workflow
    - Auto-updates formula on tag creation
    - Calculates SHA256 hash automatically
    - Adds revision field to force Homebrew updates
    - Eliminates manual formula maintenance

--- Formula/transcriber-pro.rb
+++ Formula/transcriber-pro.rb
@@ -2,9 +2,9 @@ class TranscriberPro < Formula
   desc "Privacy-first audio transcription using Whisper Large-v3"
   homepage "https://github.com/hnrqer/transcriber-pro"
   url "https://github.com/hnrqer/transcriber-pro/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "650be81b48cfdb1df71cd2f06c3c85ec95945ed657352c99e80854290807150d" # Will be updated after creating the tag
+  sha256 "650be81b48cfdb1df71cd2f06c3c85ec95945ed657352c99e80854290807150d"
   license "MIT"
-
+  revision 1
   depends_on "go" => :build
   depends_on "cmake" => :build
   depends_on "ffmpeg"
